a595dd749a69286582795e1011c23b49fc8df67a
sunqiao <sunqiao@kayak.com.cn>
2019-08-01 10:50:30 +0800

    修复一些问题

--- src/application/config/Config.java
+++ src/application/config/Config.java
@@ -8,6 +8,6 @@ public class Config {
 	public static final String APP_VERSION = " V 1.0.2";
 	public static final int APP_VERSION_CODE = 10;
 
-	//public static final String BASE_URL = "http://118.24.241.167:8081";
-	public static final String BASE_URL = "http://localhost:8081";
+	public static final String BASE_URL = "http://118.24.241.167:8081";
+	//public static final String BASE_URL = "http://localhost:8081";
 }
